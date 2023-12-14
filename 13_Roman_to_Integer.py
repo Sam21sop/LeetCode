3ffc37699fe6c95f7bcf8296f197f08b9a1abaa0
Sam21sop <sbussiness21@gmail.com>
2023-12-14 18:47:54 +0530

    roman to integer numerical value

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -1,17 +1,31 @@
 class Solution:
-    symbols = {
-        'I' : 1,
-        'V' : 5,
-        'X' : 10,
-        'L' : 50,
-        'C' : 100,
-        'D' : 500,
-        'M' : 1000
-    }
-
 
     def romanToInt(self, s: str) -> int:
-        pass
+        symbols = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+        count = 0
+        index = 0
+        while index < len(s):
+            ch1 = symbols[s[index]]
+            if index + 1 < len(s):
+                ch2 = symbols[s[index + 1]]
+                if ch1 >= ch2:
+                    count += ch1
+                    index += 1
+                else:
+                    count = count + ch2 - ch1
+                    index += 2
+            else:
+                count += ch1
+                index += 1
+        return count
 
 
 
